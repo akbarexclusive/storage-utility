07b4651bc8b6f7641ebe2f3b6aeda59fa4065183
Shubham Kesarwani <shubham.kesarwani89@gmail.com>
2018-07-25 16:38:54 +0530

    Update storage package v0.0.7

---
+++ README.md
@@ -0,0 +1,45 @@
+# STORAGE UTILITY 0.0.1 #
+ 
+Target - Front End Projects
+
+* Supported Storage Engines - localStorage, React-Native's AsyncStorage
+
+### How do I get set up? ###
+
+* Run 'npm i storage-utility --save'
+* If using in React Native, setup environment by calling InitializeStorageUtils
+```
+	InitializeStorageUtils({
+        storeName: 'Test', // default - 'STORAGE_UTILITY'
+        engine: AsyncStorage, // default - localStorage
+        STORAGE_ENGINE_NAME: 'AsyncStorage' // identifier, default - 'localStorage'
+    })
+```
+
+### Sample 
+```
+    import React , {Component} from 'react';
+    import { GetItem, SetItem } from 'storage-utility';
+
+    export default class StorageTest {
+        constructor(props) { 
+            SetItem('testVar', {anything: 'any value'});
+            console.log(GetItem('testVar'));
+        }   
+    }
+
+```
+
+### APIs ###
+
+* InitializeStorageUtils - setup env for package (can be skipped in case of web )
+* SetItem('key', 'value') - Stores under volatile category
+* GetItem('key') - Stores under non volatile category i
+* SetNonVolatileItem('key', 'value') - Stores under nonvolatile category
+* RemoveItem({ clearVolatileStorage = true, clearNonVolatileStorage = false }) - Removes item from particular category. 
+
+
+### Document Author ###
+  [Shubham_Kesarwani](https://github.com/shubhamkes)
+
+ 
---
+++ src/storage.utils.js
@@ -0,0 +1,143 @@
+
+/********************************************************
+ * Provides Methods for Storing data which is persistent
+ * Currently supports two engines
+ * (localStorage, React-Native's AsyncStorge)
+ * 
+ * to use this package for AsyncStorage one has to initialize 
+ * configuration 
+ * 
+ * Ex - InitializeStorageUtils({
+ *      AsyncStorage, storeName, engine
+ * })
+ *******************************************************/
+
+let STORAGE_ENGINE_NAME;
+
+let AsyncStorage;
+let volatile = {};
+let nonVolatile = {};
+const ENGINES = ['AsyncStorage', 'localStorage']
+
+let env = {
+    STORE_NAME: '', STORAGE_ENGINE_NAME: ''
+}
+
+InitializeStorageUtils({});
+
+export function InitializeStorageUtils({ engine, storeName, engineName }) {
+    env = {
+        STORE_NAME: storeName || 'STORAGE_UTILITY',
+        STORAGE_ENGINE_NAME: engineName || 'localStorage',
+        ENGINE: engine || window ? window.localStorage : 1 && console.warn('Could not find Storage Engine. Cant use storage apis')
+    }
+    setDefault();
+}
+
+async function setDefault() {
+    let vol, nonVol;
+    switch (env.STORAGE_ENGINE_NAME) {
+        case 'AsyncStorage':
+            Promise.all([storageUtils({ method: 'getItem', key: 'volatile' }), storageUtils({ method: 'getItem', key: 'nonVolatile' })])
+                .then(res => {
+                    [vol, nonVol] = [res[0], res[1]];
+                });
+            break;
+
+        case 'localStorage':
+            [vol, nonVol] = [localStorage.volatile, localStorage.nonVolatile];
+            break;
+    }
+    try {
+        volatile = vol ? JSON.parse(vol) : {};
+        nonVolatile = nonVol ? JSON.parse(nonVol) : {};
+    } catch (e) { console.error(e); }
+}
+
+/**
+ * Sets item in localStorage under 'volatile' keyword
+ * @param  {string} key
+ * @param  {any} payload 
+ */
+export function SetItem(key, payload) {
+    if (IsUndefined(payload)) {
+        delete volatile[key];
+    }
+    else {
+        volatile[key] = payload;
+    }
+
+    storageUtils({ method: 'setItem', key: 'volatile', payload: JSON.stringify(volatile) });
+}
+
+/**
+ * Returns data for particular key
+ * @param  {string} key 
+ * @param  {boolean} nonVolatile - (optional)
+ */
+export function GetItem(key, nonVolatile = false) {
+    if (!key) {
+        return null;
+    }
+
+    if (nonVolatile) {
+        return nonVolatile[key];
+    }
+    return volatile[key];
+
+}
+
+/**
+ * Same as SetItem, only difference is nonVolatile item is not wiped off even after logout
+ * @param  {string} key
+ * @param  {any} payload
+ */
+export function SetNonVolatileItem(key, payload) {
+    if (IsUndefined(payload))
+        delete nonVolatile[key];
+    else
+        nonVolatile[key] = payload;
+
+    storageUtils({ method: 'setItem', key: 'nonVolatile', payload: JSON.stringify(nonVolatile) }, true);
+}
+
+/**
+ * Removes localstorage value
+ * based on parameter, can remove particular key or whole volatile or nonVolatile storage from localStorage
+ * @param  {boolean} clearLocalStorage - (optional)
+ * @param  {boolean} clearNonVolatileStorage} - (optional)
+ */
+export function RemoveItem({ key, clearVolatileStorage = true, clearNonVolatileStorage = false }) {
+    if (clearVolatileStorage) {
+        volatile = {};
+        key = 'volatile';
+    }
+    if (clearNonVolatileStorage) {
+        nonVolatile = {};
+        key = 'nonVolatile';
+    }
+    storageUtils({ method: 'removeItem', key });
+}
+
+function storageUtils({ method, key, payload }, shouldParse) {
+    try {
+        payload = shouldParse ? JSON.parse(payload) : payload;
+        env.ENGINE[method](resolveKey(key), payload);
+    } catch (e) {
+        console.warn('Something went wrong with storage utils');
+        console.error(e);
+    }
+}
+
+
+export function resolveKey(key) {
+    if (IsUndefined(key)) {
+        return key;
+    }
+    return env.STORAGE_ENGINE_NAME == 'AsyncStorage' ? `${env.STORE_NAME}:${key}` : key;
+}
+
+function IsUndefined(value) {
+    return typeof value == 'undefined';
+    // return value === '';
+}
