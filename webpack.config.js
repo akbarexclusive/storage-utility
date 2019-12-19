cd9252af013718800cb24fccf9e7ea728e860803
Shubham Kesarwani <shubham.kesarwani@drivezy.com>
2019-12-19 20:49:01 +0530

    1.0.0 stable release

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
-/*** webpack.config.js ***/
-const path = require('path');
+var path = require('path');
 
 module.exports = {
-    // entry: path.join(__dirname, "src/index.js"),
-    entry: path.join(__dirname, "src/index.js"),
+    entry: './src/index.js',
     output: {
-        path: path.join(__dirname, "/build"),
-        filename: "index.js"
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    resolve: {
+        extensions: ['.js'],
     },
     module: {
         rules: [
             {
-                test: /\.(js)$/,
-                use: "babel-loader",
-                exclude: /node_modules/
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
             }
         ]
-    },
-    resolve: {
-        extensions: [".js"]
-    },
-    devServer: {
-        port: 3001
     }
 };
+
